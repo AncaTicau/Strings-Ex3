28e8be7ca168cc1a9617de040e9beecf5ab9f400
MariusA <anca.ticau@gmail.com>
2019-06-06 22:46:39 +0300

    Exercise3

---
+++ Strings-Ex3/Strings-Ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Strings_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sumOfDigits = SumOfDigits("A5d6b8");
+
+
+            Console.WriteLine(sumOfDigits);
+
+            Console.ReadKey();
+        }
+
+        // Write a method to compute sum of digits of a given string(if any).
+
+        public static int SumOfDigits(string givenString)
+        {
+            int result = 0;
+
+            for (int i = 0; i < givenString.Length; i++)
+            {
+
+                if (!int.TryParse(givenString[i].ToString(), NumberStyles.None, CultureInfo.InvariantCulture, out int digit))
+                {
+                    continue;
+                }
+
+                result += digit;
+
+            }
+
+            return result;
+
+        }
+    }
+}
